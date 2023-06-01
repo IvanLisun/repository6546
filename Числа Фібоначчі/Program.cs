9bd8833bc3e81b5fcb27c016e313f22101b53c2e
Lisun Ivan <vanlisun@ukr.net>
2023-06-01 13:43:47 +0300

    43543214

---
+++ Числа Фібоначчі/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Числа_Фібоначчі
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = 0;
+            int n2 = 1;
+            Console.Write(n1 + " " + n2 + " ");
+            for (int i = 2; i < 38; i++)
+            {
+                int n3 = n1 + n2;
+                n1 = n2;
+                n2 = n3;
+                Console.Write(n3 + " ");
+            }
+            Console.Read();
+        }
+    }
+}
